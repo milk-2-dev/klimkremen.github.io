6496f067f2ca2b7abace593bd8714c9a421baa77
Roman <papachiv.roma@gmail.com>
2017-12-16 20:02:31 +0200

    add valid js

--- projects/sotnik-learning/js/main.js
+++ projects/sotnik-learning/js/main.js
@@ -29,118 +29,24 @@ function customCollapse(){
 
 customCollapse();
 
-jQuery(function($){
-    $("#phone").mask("+38(999) 999-9999");
-});
-
-var smallBooks = d3.select('#smallBooks'),
-    drawing = d3.select('#drawing'),
-    book,
-    tween,
-    delay,
-    timeline;
-
-function init() {
-    var i, x, y, z,
-        tweens = [],
-        book,
-        delay;
-
-    for (i = 0; i < 20; i += 1) {
-        addBook(false);
-    }
-
-    for (i = 0; i < 10; i += 1) {
-        x = 700 + Math.sin(i) * (200 - i * 15);
-        y = 240 + Math.cos(i) * (200 - i * 15);
-        z = i * -100;//Math.random() * -200;
-        book = addFlippableBook(x, y);
-        delay = Math.random() * 5;
-        TweenMax.set(book, {css: {x: x, y: y, z: z, rotationY: -45}});
-
-        tween = TweenMax.to(book, 3 + Math.random() * 3, {
-            css: {
-                y: y + 10 - Math.random() * 20,
-                x: x +  10 - Math.random() * 20,
-                z: z + 10 - Math.random() * 20,
-                rotationX: Math.random() * 100,
-                rotationY: -45 + Math.random() * 10},
-            delay: delay,
-            ease: Quint.EaseInOut,
-            yoyo: true,
-            repeat: -1});
-
-        tweens.push(tween);
-
-        var k = book.find('.page').length,
-            rotation = 5 + Math.random() * 30,
-            openSpeed = 2 + Math.random();
-
-        book.find('.page').each(function (i) {
-            tween = TweenMax.to($(this), openSpeed, {
-                css: {
-                    rotationY: -((k - i) * rotation),
-                    className: "dark"},
-                delay: delay,
-                yoyo: true,
-                repeat: -1});
-            tweens.push(tween);
-        });
-    }
-
-    timeline = new TimelineMax({tweens: tweens});
-
-    TweenMax.to($('body'), 0.5, {css:{opacity: 1}, delay: 0.5});
-}
-
-function addFlippableBook(x, y) {
-    var $book = $('<div class="book">');
-    //$book.css('webkitTransform', 'translate(' + x + 'px, ' + y + 'px)')
-
-    for (var i = 0; i < 5; i += 1) {
-        var $page = $('<div class="page">');
-        //$page.css('webkitTransform', 'translateZ(' + i + 'px) translateX(' + i + 'px)');
-
-        if (i == 0 || i == 4) {
-            $page.addClass('cover');
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    dots: false,
+    nav: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
         }
-
-        $book.append($page);
     }
+})
 
-    $('#bookContainer').append($book);
-    return $book;
-}
 
-function addBook(small) {
-    var scale = small ? 0.4 : 0.5 + Math.random() - 0.25,
-        r1 = Math.random(),
-        r2 = Math.random(),
-        x = 700 + (r1 * 400) - 200,
-        y = 240 + (r2 * 400) - 200;
-
-    smallBooks.append("rect")
-        .attr("class", "small-rect")
-        .attr("x", -25)
-        .attr("y", -35)
-        .attr("width", 56)
-        .attr('height', 69)
-        .attr('transform', 'translate(' + x + ' ' + y + ') scale(' + scale + ')')
-        .attr('opacity', .5)
-        .attr('fill', '#619EFB')
-        .transition()
-        .duration(1000 + Math.random() * 500)
-        .ease('linear')
-        .attr('opacity', '.0')
-        .attr('transform', 'translate(' + x + ' ' + y + ') scale(1.0)')
-        .each('end', function () {
-            this.remove();
-            addBook(true);
-        });
-}
-
-function update() {
-
-}
-
-init();
+// jQuery(function($){
+//     $("#phone").mask("+38(999) 999-9999");
+// });
