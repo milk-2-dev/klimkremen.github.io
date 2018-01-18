51b1000f5a07537fc75963ec097456a257f05a07
Roman <papachiv.roma@gmail.com>
2018-01-18 21:30:11 +0200

    2 versions add

---
+++ projects/analytics/version_2/js/main.js
@@ -0,0 +1,29 @@
+function animateMobile(){
+    var animateAddClass = setInterval(function() {
+        $('.header-number i').addClass('tada');
+    }, 3000);
+
+    var animateRemoveClass = setInterval(function() {
+        $('.header-number i').removeClass('tada');
+    }, 6000);
+
+}
+
+animateMobile();
+
+$(document).ready(function(){
+    $('#accordion .card').each(function(){
+        $(this).find('h5').hover(function () {
+
+                var blockId = $(this).find('.collapsed').attr('href');
+                //console.log(blockId);
+
+            $(blockId).on('show.bs.collapse', function () {
+                $('#accordion').find('div[aria-expanded="true"]').collapse('hide');
+            })
+                $(this).parent().parent().find(blockId).collapse('show');
+
+
+        })
+    })
+});
